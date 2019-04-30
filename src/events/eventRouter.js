fec14fd5bc5d9eb3edea54510e7ee1ddec29f61a
JizongL <2015rpro@gmail.com>
2019-04-30 15:58:59 -0400

    post endpoint is setup, and postman can query successfully

--- src/events/eventRouter.js
+++ src/events/eventRouter.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const eventRouter = express.Router()
 const jsonBodyParser = express.json()
-const StressEventsSerivce = require('./userEventsService')
+const StressEventsService = require('./userEventsService')
 const { requireAuth } = require('../middleware/jwt-auth')
+const path = require('path')
 
 eventRouter
   .route('/')
   .get(requireAuth)
-  .get((req,res,next)=>{
-    
+  .get((req,res,next)=>{    
   const knexInstance = req.app.get('db')
-  StressEventsSerivce.getAllEvents(knexInstance,req.user.id)
+  StressEventsService.getAllEvents(knexInstance,req.user.id)
   .then(events=>{
     res.json(events)
   })
@@ -18,8 +18,28 @@ eventRouter
 
   
 })
-.post(jsonBodyParser,(req,res,next)=>{
-
+.post(requireAuth,jsonBodyParser,(req,res,next)=>{
+  const {stress_event,mood,work_efficiency,stress_cause,stress_score,symptoms,coping} = req.body
+  const newEvent = {stress_event,mood,work_efficiency,stress_cause,stress_score,symptoms,coping}
+  newEvent.user_id = req.user.id
+  console.log(newEvent,'test new event')
+  for (const [key, value] of Object.entries(newEvent))
+      if (value == null)
+        return res.status(400).json({
+          error: `Missing '${key}' in request body`
+        })
+        StressEventsService.insertEvent(
+    req.app.get('db'),
+    newEvent
+  )
+  .then(event=>{
+    console.log(event.id,'test event inside event router')
+    res
+      .status(201)
+      .location(path.posix.join(req.originalUrl, `/${event.id}`))   
+      .json(event) 
+  })
+  .catch(next)
 })
 
 
--- src/events/userEventsService.js
+++ src/events/userEventsService.js
@@ -1,21 +1,71 @@
 const xss = require('xss')
 
-const StressEventsSerivce = {
+const StressEventsService = {
   getAllEvents(db,userId){
     return db
-      .select('*')
+      
       .from('stress_events AS eve')
+      .select(
+        'usr.full_name',
+        'usr.id',
+        'eve.stress_event',
+        'eve.user_id',
+        'eve.mood',
+        'eve.work_efficiency',
+        'eve.stress_cause',
+        'eve.symptoms',
+        'eve.coping',
+        'eve.date_recorded',
+
+        )
       .join('stress_users AS usr',
-          'eve.id',
+          'eve.user_id',
           'usr.id'
       )      
       .where('eve.user_id',userId)
 
   },
-  getById(db,userId,id){
+  insertEvent(db,newEvent){
+    return db
+      .insert(newEvent)
+      .into('stress_events')
+      .returning('*')
+      .then(([event]) => event)
+      .then(event =>{
+        console.log(event.id,'test event in event service')
+        return StressEventsService.getById(db,event.id)
+
+      })
+  },
+  // need to test if userId is needed
+  getById(db,id){
+    console.log(id,'test id inside get by id')
+    return db
+    .from('stress_events AS eve')
     
+      .select(
+        'usr.full_name',
+        'usr.id',
+        'eve.id',        
+        'eve.user_id',
+        'eve.stress_event',
+        'eve.mood',
+        'eve.work_efficiency',
+        'eve.stress_cause',
+        'eve.symptoms',
+        'eve.coping',
+        'eve.date_recorded',
+
+        )
+      .join('stress_users AS usr',
+          'eve.user_id',
+          'usr.id'
+      )      
+       .where('eve.id',id)
+    
+
   }
 
 }
 
-module.exports = StressEventsSerivce
+module.exports = StressEventsService
