b82a4c09505707254710976fa6822b953cb18c56
JizongL <2015rpro@gmail.com>
2019-05-09 16:08:55 -0400

    clean up the code

--- src/events/eventRouter.js
+++ src/events/eventRouter.js
@@ -5,29 +5,27 @@ const StressEventsService = require('./userEventsService')
 const { requireAuth } = require('../middleware/jwt-auth')
 const path = require('path')
 
-// remember the events.map() here, I was trying to 
-// res.json(StressEventsService.serializeEvent(events)) before, it didn't
-// work, because events are an array of objects. 
+// event routes with no id specified
+// GET and POST events 
 eventRouter
   .route('/')
   .get(requireAuth)
   .get((req,res,next)=>{    
-  const knexInstance = req.app.get('db')
-  StressEventsService.getAllEvents(knexInstance,req.user.id)
+    const knexInstance = req.app.get('db')
+    StressEventsService.getAllEvents(knexInstance,req.user.id)
   .then(events=>{
     res.json(events.map(StressEventsService.serializeEvent))
   })
-  .catch(next)
-
-  
+  .catch(next)  
 })
-.post(requireAuth,jsonBodyParser,(req,res,next)=>{
-  const {stress_event,mood,work_efficiency,stress_cause,stress_score,symptoms,coping} = req.body
-  const newEvent = {stress_event,mood,work_efficiency,stress_cause,stress_score,symptoms,coping}
-  newEvent.user_id = req.user.id
+
+  .post(requireAuth,jsonBodyParser,(req,res,next)=>{
+    const {stress_event,mood,work_efficiency,stress_cause,stress_score,symptoms,coping} = req.body
+    const newEvent = {stress_event,mood,work_efficiency,stress_cause,stress_score,symptoms,coping}
+    newEvent.user_id = req.user.id
   
 
-
+// verify all fields have value
   for (const [key, value] of Object.entries(newEvent))
       if (value == null)
         return res.status(400).json({
@@ -35,16 +33,16 @@ eventRouter
         })
   stressEvent_input_error = StressEventsService.validateStressEventInput(stress_event)
   
+  // validate stress event title does not exceed a certain length  
   if(stressEvent_input_error)
       res.status(400).json({ error: stressEvent_input_error })
-        
-        
+
+      // insert into event database                
     StressEventsService.insertEvent(
     req.app.get('db'),
     newEvent
   )
   .then(event=>{
-    //console.log(event.id,'test event inside event router')
     res
       .status(201)
       .location(path.posix.join(req.originalUrl, `/${event.id}`))   
@@ -53,21 +51,21 @@ eventRouter
   .catch(next)
 })
 
+// DELETE and PATCH event given an id
 eventRouter
   .route('/:event_id')
+  // .all take care everything before delete and patch
   .all(requireAuth)
   .all((req,res,next)=>{
     StressEventsService.getById(
       req.app.get('db'),
       req.params.event_id
     )
-    .then(event=>{
-      // console.log(event,'test event')
+    .then(event=>{      
       if(!event){
         return res.status(404).json(
           {error:{message:`event doesn't exist`}}
         )
-
       }
       
       res.event = event
