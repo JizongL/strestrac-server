abeeb3b7ba31f238c470501c75e334f5358d1192
JizongL <2015rpro@gmail.com>
2019-05-09 16:55:27 -0400

    updated config.js on DATABASE_URL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin@localhost/strestrac',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/strestrac',
   JWT_SECRET: process.env.JWT_SECRET || '395d92cf-e93c-4945-8578-0cffa7181d9a',
 }
 
